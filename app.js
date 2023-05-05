0356a1a6a068a67d1bee605254a635197527cbe2
gaozhengxin <zhengxingao@live.cn>
2023-05-05 16:22:45 +0800

    add liquidity check

--- app.js
+++ app.js
@@ -5,29 +5,49 @@ const { Level } = require('level');
 const db_tokenInfos = new Level("tokenInfos");
 const db_trustedBridges = new Level("trustedBridges");
 
-const trustedGatewayCodeHash = new Map();
-trustedGatewayCodeHash.set("0x2a81d1cd005c72ef885fb17449d1067fb1136d38ddfea365e7efd3f452b3b423", 1); // gateway mint burn v1
-trustedGatewayCodeHash.set("0x823698c71e40c852f7484c667be7c7d78c8818ae0daf844650fc5f287bff560c", 1); // gateway pool v1
+//const trustedGatewayCodeHash = new Map();
+//trustedGatewayCodeHash.set("0x2a81d1cd005c72ef885fb17449d1067fb1136d38ddfea365e7efd3f452b3b423", 1); // gateway mint burn v1
+//trustedGatewayCodeHash.set("0x823698c71e40c852f7484c667be7c7d78c8818ae0daf844650fc5f287bff560c", 1); // gateway pool v1
 
-const trustedTokenCodeHash = new Map();
-trustedTokenCodeHash.set("0x149371ad26463543075824dfabdff189653d9496348d4a029b52395c5f08d4de", 1); // bridge token
+//const trustedTokenCodeHash = new Map();
+//trustedTokenCodeHash.set("0x149371ad26463543075824dfabdff189653d9496348d4a029b52395c5f08d4de", 1); // bridge token
 
 var providers = {}
-providers["5"] = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth_goerli");
-providers["97"] = new ethers.JsonRpcProvider("https://rpc.ankr.com/bsc_testnet_chapel");
-providers["4002"] = new ethers.JsonRpcProvider("https://rpc.ankr.com/fantom_testnet");
 
+const initProvider = async (chainId, url) => {
+    console.log(`chainid : ${chainId}, url : ${url}`);
+    for (; true;) {
+        try {
+            //providers[chainId] = new ethers.JsonRpcProvider(url);
+            const provider = new ethers.JsonRpcProvider(url);
+            return provider;
+        } catch (error) {
+            console.warn(`create provider error : ${JSON.stringify({ chainid: chainId, error: error })}`);
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            continue;
+        }
 
-const checkChainIDs = async () => {
-    Object.keys(providers).forEach(async (key) => {
-        const { chainId } = await providers[key].getNetwork()
-        console.log(`chain id : ${chainId}`);
-    });
-};
+    }
+}
 
-//checkChainIDs();
+const initProviders = async () => {
+    (async () => { providers["5"] = await initProvider("5", "https://rpc.ankr.com/eth_goerli"); })();
+    (async () => { providers["97"] = await initProvider("97", "https://rpc.ankr.com/bsc_testnet_chapel"); })();
+    (async () => { providers["4002"] = await initProvider("4002", "https://rpc.ankr.com/fantom_testnet"); })();
+    (async () => { providers["1287"] = await initProvider("1287", "https://rpc.api.moonbase.moonbeam.network"); })();
+    (async () => { providers["43113"] = await initProvider("43113", "https://rpc.ankr.com/avalanche_fuji"); })();
+    (async () => { providers["420"] = await initProvider("420", "https://optimism-goerli.public.blastapi.io"); })();
+    (async () => { providers["421613"] = await initProvider("421613", "https://goerli-rollup.arbitrum.io/rpc"); })();
+}
+
+initProviders();
 
 const admin = "0x7fa4b6F62fF79352877B3411Ed4101C394a711D5";
+
+/*
+1. create ethers rpc provider
+2. get gateway balance
+*/
 
 // create a server
 const server = new jayson.Server({
@@ -65,9 +85,21 @@ const server = new jayson.Server({
         }
     },
     fax_getTokenInfos: async (args, callback) => {
+        var checkStatus = false;
+        switch (args[0]) {
+            case true:
+                checkStatus = true;
+            case "true":
+                checkStatus = true;
+            default:
+        }
         try {
             var tokenInfos = [];
             for await (const id of args) {
+                if (id === true || id === "true" || id === false || id === "false") {
+                    continue;
+                }
+
                 const value = await db_tokenInfos.get(id);
                 var tokenInfo = formatTokenInfo(JSON.parse(value));
 
@@ -85,6 +117,26 @@ const server = new jayson.Server({
                     tokenInfo.configs[i].maxTxAmount = "";
                 }
                 tokenInfos.push(tokenInfo);
+            }
+            if (checkStatus) {
+                for (var i = 0; i < tokenInfos.length; i++) {
+                    for (var j = 0; j < tokenInfos[i].configs.length; j++) {
+                        if (tokenInfos[i].configs[j].type == "pool") {
+                            // check balance
+                            const calldata = "0x70a08231000000000000000000000000" + tokenInfos[i].configs[j].gateway.toLowerCase().replace("0x", "");
+                            try {
+                                if (providers[tokenInfos[i].configs[j].chainId] !== undefined && providers[tokenInfos[i].configs[j].chainId] !== null) {
+                                    let bal = await providers[tokenInfos[i].configs[j].chainId].call({ to: tokenInfos[i].configs[j].token, data: calldata });
+                                    tokenInfos[i].configs[j].liquidity = parseInt(bal, 16).toString();
+                                } else {
+                                    tokenInfos[i].configs[j].liquidity = JSON.stringify({ error: {} });
+                                }
+                            } catch (error) {
+                                tokenInfos[i].configs[j].liquidity = JSON.stringify({ error: error });
+                            }
+                        }
+                    }
+                }
             }
             callback(null, tokenInfos);
         } catch (error) {
@@ -140,10 +192,10 @@ const server = new jayson.Server({
                     configs: args[0].configs
                 }
                 //var payload = formatTokenInfo(tokenInfo);
-                console.log(`payload : ${JSON.stringify(payload)}`);
+                console.log(`payload: ${JSON.stringify(payload)} `);
 
                 var res = ethers.verifyMessage(JSON.stringify(payload), args[0].signature);
-                console.log(`res : ${res}`);
+                console.log(`res: ${res} `);
                 if (res !== deployer) {
                     callback(null, "fail to verify signature");
                     return;
@@ -184,11 +236,11 @@ const formatTokenInfo = (tokenInfo1) => {
 
 const checkTrustedAddresses = async (tokenInfo) => {
     for (const key in tokenInfo.configs) {
-        console.log(`key : ${key}`);
-        var trustedGateway = await isStandardGateway(key, tokenInfo.configs[key].gateway);
-        var trustedToken = await isStandardBridgeToken(key, tokenInfo.configs[key].token);
-        console.log(`trustedGateway : ${trustedGateway}`);
-        console.log(`trustedToken : ${trustedToken}`);
+        console.log(`key: ${key} `);
+        //var trustedGateway = await isStandardGateway(key, tokenInfo.configs[key].gateway);
+        //var trustedToken = await isStandardBridgeToken(key, tokenInfo.configs[key].token);
+        console.log(`trustedGateway: ${trustedGateway} `);
+        console.log(`trustedToken: ${trustedToken} `);
         if (trustedGateway && trustedToken) {
             tokenInfo.configs[key].isTrustedGateway = true;
             tokenInfo.configs[key].isTrustedBridgeToken = true;
@@ -208,23 +260,17 @@ const checkTrustedAddresses = async (tokenInfo) => {
 }
 
 const isStandardGateway = async (chainid, gateway) => {
-    let code = await providers[chainid].getCode(gateway);
-    const hash = ethers.keccak256(code);
     return true;
-    console.log(hash);
-    console.log(trustedGatewayCodeHash[hash]);
-    console.log(trustedGatewayCodeHash[hash] === 1);
-    return (trustedGatewayCodeHash[hash] === 1);
+    //let code = await providers[chainid].getCode(gateway);
+    //const hash = ethers.keccak256(code);
+    //return (trustedGatewayCodeHash[hash] === 1);
 }
 
 const isStandardBridgeToken = async (chainid, token) => {
-    let code = await providers[chainid].getCode(token);
-    const hash = ethers.keccak256(code);
     return true;
-    console.log(hash);
-    console.log(trustedTokenCodeHash[JSON.stringify(hash)]);
-    console.log(trustedTokenCodeHash[hash] === 1);
-    return (trustedTokenCodeHash[hash] === 1);
+    //let code = await providers[chainid].getCode(token);
+    //const hash = ethers.keccak256(code);
+    //return (trustedTokenCodeHash[hash] === 1);
 }
 
 function containsSpecialChars(str) {
